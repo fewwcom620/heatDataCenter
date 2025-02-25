876dc8ea8ae44c319d0059ee2d2d37f9464673d5
fewwcom <fewwcom620@gmail.com>
2025-02-25 14:26:06 +0800

    get ports

--- src/main/java/com/wistron/vdu300/heatdataagent/controller/DataTransferController.java
+++ src/main/java/com/wistron/vdu300/heatdataagent/controller/DataTransferController.java
@@ -1,9 +1,12 @@
 package com.wistron.vdu300.heatdataagent.controller;
 
+import com.fazecast.jSerialComm.SerialPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,4 +20,23 @@ public class DataTransferController {
         return results;
     }
 
+    @GetMapping("/ports")
+    public Map ports() {
+        HashMap<String, Object> results = new HashMap<String, Object>();
+
+        SerialPort[] commPorts = SerialPort.getCommPorts();
+
+        for (SerialPort commPort : commPorts) {
+
+        }
+
+        List<String> names = Arrays.stream(commPorts).map(SerialPort::getDescriptivePortName).toList();
+
+        List<String> descriptions = Arrays.stream(commPorts).map(SerialPort::toString).toList();
+
+        results.put("portName", names);
+        results.put("description", descriptions);
+
+        return results;
+    }
 }
